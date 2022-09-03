8627fcfedcd957b4d0146bab74558a951857314b
Ramdeni Akshith <akshithramdeni6@gmail.com>
2022-09-03 07:53:29 +0000

    Update Expenseitem.js and notes.txt

--- src/Component/Expenseitem.js
+++ src/Component/Expenseitem.js
@@ -1,8 +1,16 @@
+ import './Expenseitem.css';
  function Expenseitem(){
+     const expenseDate = new Date(28,3,17);
+     const expenseTitle = 'Car Insurance';
+     const expenseAmount = 456;
     return(
-        <div>
-            <h2>Expenses items.</h2>
+       <div className='expense-item '>
+        <div>{expenseDate.toISOString()}</div>
+        <div className='expense-item__description'>
+            <h2>{expenseTitle}</h2>
+            <div className='expense-item__price'>${expenseAmount}</div>
         </div>
+       </div>
     );
 }
 
