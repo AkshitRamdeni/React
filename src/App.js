2ee088561db8e44b6b72818723e6f19cd0bd515f
Ramdeni Akshith <akshithramdeni6@gmail.com>
2022-09-03 06:16:28 +0000

    Reading First Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Expenseitem from './Component/Expenseitem';
+
 function App(){
     return(
       <div>
         <h2>lets get started.</h2>
-        {/* <marquee>Hello World</marquee> */}
+        <Expenseitem></Expenseitem>
       </div>
     );
 }
